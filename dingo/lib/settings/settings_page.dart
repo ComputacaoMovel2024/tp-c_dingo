9b281c9ff44db74611e365b6764f9d5205152011
DavidRSantana <202200215@estudantes.ips.pt>
2024-06-10 20:00:09 +0100

    Correção do último commit

--- dingo/lib/settings/settings_page.dart
+++ dingo/lib/settings/settings_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:dingo/dart_classes_aux/custom_slider.dart';
-import 'package:dingo/dart_classes_aux/custom_button.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -31,49 +30,62 @@ class SettingsPage extends StatelessWidget {
                 backgroundColor: Colors.transparent,
                 elevation: 0,
               ),
-              const Expanded(
+              Expanded(
                 child: Center(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     mainAxisSize: MainAxisSize.min,
                     children: <Widget>[
-                      CustomSlider(
+                      const CustomSlider(
                         customIconPath: 'lib/assets/music_note_icon.png',
                         preferenceKey: 'musicNoteSliderValue', //chave para as Shared Preferences
                       ),
-                      CustomSlider(
+                      const CustomSlider(
                         customIconPath: 'lib/assets/speaker_icon.png',
                         preferenceKey: 'speakerSliderValue', //chave para as Shared Preferences
                       ),
-                      Divider(
-                        thickness: 1,
+                      const Divider(
+                        thickness: 1.5,
                         color: Colors.black,
                         indent: 40,
                         endIndent: 40,
                       ),
+                      const SizedBox(height: 5),
                       CustomSettingsButton(
-                        settingsIcon: Icon(Icons.person, size: 30), 
+                        settingsIcon: const Icon(Icons.person, size: 30), 
                         settingsText: 'Edit Profile',
+                        onButtonPressed: () {},
                       ),
-                      SizedBox(height: 5),
+                      const SizedBox(height: 5),
                       CustomSettingsButton(
-                        settingsIcon: Icon(Icons.language, size: 30), 
+                        settingsIcon: const Icon(Icons.language, size: 30), 
                         settingsText: 'Change Language',
+                        onButtonPressed: () {},
                       ),
-                      SizedBox(height: 5),
+                      const SizedBox(height: 5),
                       CustomSettingsButton(
-                        settingsIcon: Icon(Icons.lock, size: 30), 
+                        settingsIcon: const Icon(Icons.lock, size: 30), 
                         settingsText: 'Privacy Settings',
+                        onButtonPressed: () {},
                       ),
-                      SizedBox(height: 5),
+                      const SizedBox(height: 5),
                       CustomSettingsButton(
-                        settingsIcon: Icon(Icons.headset_mic, size: 30), 
+                        settingsIcon: const Icon(Icons.headset_mic, size: 30), 
                         settingsText: 'Help & Support',
+                        onButtonPressed: () {},
                       ),
-                      SizedBox(height: 5),
+                      const SizedBox(height: 5),
                       CustomSettingsButton(
-                        settingsIcon: Icon(Icons.info, size: 30), 
+                        settingsIcon: const Icon(Icons.info, size: 30), 
                         settingsText: 'Info & Credits',
+                        onButtonPressed: () {},
+                      ),
+                      const SizedBox(height: 5),
+                      const Divider(
+                        thickness: 1.5,
+                        color: Colors.black,
+                        indent: 40,
+                        endIndent: 40,
                       ),
                     ],
                   ),
@@ -82,6 +94,56 @@ class SettingsPage extends StatelessWidget {
             ],
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CustomSettingsButton extends StatelessWidget {
+  final Icon settingsIcon;
+  final String settingsText;
+  final VoidCallback? onButtonPressed;
+
+  const CustomSettingsButton({
+    super.key,
+    required this.settingsIcon,
+    required this.settingsText,
+    required this.onButtonPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onButtonPressed,
+      child: Container(
+        width: 300,
+        height: 60,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 202, 231, 255),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            settingsIcon,
+            const Spacer(),
+            Center(
+              child: Text(
+                settingsText,
+                textAlign: TextAlign.left,
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            const Spacer(),
+            const Icon(
+              Icons.arrow_forward,
+              size: 30,
+            ),
+          ],
+        ),
       ),
     );
   }
