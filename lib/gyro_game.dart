5b0cc7e85e0219a5158a240f5ecc1c905d4bd4f5
diogopoupinha-ips <202200184@estudantes.ips.pt>
2024-06-22 10:29:08 +0100

    Mudança orientação ecrã gyro_game. Ainda não testado.

--- lib/gyro_game.dart
+++ lib/gyro_game.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'header.dart';
 
 class GyroGame extends StatelessWidget {
   const GyroGame({super.key});
 
+  void main() {
+    WidgetsFlutterBinding.ensureInitialized();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+    ]);
+  }
 
   @override
   Widget build(BuildContext context) {
